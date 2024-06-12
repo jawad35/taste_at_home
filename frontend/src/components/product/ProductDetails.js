0c81f49b435ea7c367265fabe0a59ad57634d6d5
jawad35 <jaqimughal@gmail.com>
2024-06-12 11:21:47 +0500

    new changes

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -187,66 +187,68 @@ const ProductDetails = ({ match }) => {
                       <span>{product.price}</span>
                     </h4>
                     <p className="para2 mb-15">{product.description}</p>
-                    <div className="prod-quantity d-flex align-items-center justify-content-start mb-20">
-                      <div className="quantity">
-                        <input
-                          className="count p-1"
-                          type="number"
-                          value={quantity}
-                          readOnly
-                        />
-                        <div
-                          className="d-flex"
-                          style={{ flexDirection: "column" }}
-                        >
-                          <button
-                            onClick={increaseQty}
-                            style={{ border: "none", background: "none" }}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="7"
-                              height="7"
-                              fill="currentColor"
-                              className="bi bi-chevron-up"
-                              viewBox="0 0 16 16"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
-                              />
-                            </svg>
-                          </button>
-                          <button
-                            onClick={decreaseQty}
-                            style={{ border: "none", background: "none" }}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="7"
-                              height="7"
-                              fill="currentColor"
-                              className="bi bi-chevron-down"
-                              viewBox="0 0 16 16"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-                              />
-                            </svg>
-                          </button>
-                        </div>
-                      </div>
-                      <button
-                        onClick={addToCart}
-                        disabled={product.stock === 0}
-                        style={{ background: "none", border: "none" }}
+                    {
+                    user?.role !== "admin" &&  <div className="prod-quantity d-flex align-items-center justify-content-start mb-20">
+                    <div className="quantity">
+                      <input
+                        className="count p-1"
+                        type="number"
+                        value={quantity}
+                        readOnly
+                      />
+                      <div
+                        className="d-flex"
+                        style={{ flexDirection: "column" }}
                       >
-                        <a href="" className="eg-btn md--btn primary--btn">
-                          Add to cart
-                        </a>
-                      </button>
+                        <button
+                          onClick={increaseQty}
+                          style={{ border: "none", background: "none" }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="7"
+                            height="7"
+                            fill="currentColor"
+                            className="bi bi-chevron-up"
+                            viewBox="0 0 16 16"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+                            />
+                          </svg>
+                        </button>
+                        <button
+                          onClick={decreaseQty}
+                          style={{ border: "none", background: "none" }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="7"
+                            height="7"
+                            fill="currentColor"
+                            className="bi bi-chevron-down"
+                            viewBox="0 0 16 16"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
                     </div>
+                    <button
+                      onClick={addToCart}
+                      disabled={product.stock === 0}
+                      style={{ background: "none", border: "none" }}
+                    >
+                      <a href="" className="eg-btn md--btn primary--btn">
+                        Add to cart
+                      </a>
+                    </button>
+                  </div>
+                   }
                     <ul className="prod-info">
                       <li>
                         <span>Stock:</span>
